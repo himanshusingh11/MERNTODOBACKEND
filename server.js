974eaa37363aab64baf61ac089ae1d6da683241c
Himanshu <himanshu.singh_iot18@gla.ac.in>
2023-12-26 13:59:12 +0530

    added code

---
+++ server.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+
+const dbPassword = process.env.MONGODB_PASSWORD;
+const dbURI = `mongodb+srv://hshimanshusingh001:${encodeURIComponent(dbPassword)}@cluster0.ubjiaqm.mongodb.net/todoapp?retryWrites=true&w=majority`;
+
+
+
+// Connect to MongoDB Atlas
+mongoose.connect(dbURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => {
+  console.log('Connected to MongoDB Atlas');
+})
+.catch((err) => {
+  console.error('Error connecting to MongoDB Atlas:', err.message);
+});
+
+const todoSchema = new mongoose.Schema({
+  task: String,
+  completed: Boolean,
+});
+
+const Todo = mongoose.model('Todo', todoSchema);
+
+// Routes
+app.get('/api/todos', async (req, res) => {
+  try {
+    const todos = await Todo.find();
+    res.json(todos);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+app.post('/api/todos', async (req, res) => {
+  const todo = new Todo({
+    task: req.body.task,
+    completed: req.body.completed || false,
+  });
+
+  try {
+    const newTodo = await todo.save();
+    res.status(201).json(newTodo);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+app.delete('/api/todos/:id', async (req, res) => {
+  try {
+    const deletedTodo = await Todo.findByIdAndDelete(req.params.id);
+    if (!deletedTodo) {
+      return res.status(404).json({ message: 'Todo not found' });
+    }
+    res.json({ message: 'Todo deleted successfully', deletedTodo });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
+
